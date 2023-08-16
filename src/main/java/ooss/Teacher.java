611ad7e7ed9f109c6235e3add1f7d623b5eaabbb
Daphne Balane <balanedaphne20@gmail.com>
2023-08-16 19:04:14 +0800

    feat: implemented codes needed for the Step4 and passed the TeacherTest test cases

--- src/main/java/ooss/Teacher.java
+++ src/main/java/ooss/Teacher.java
@@ -6,12 +6,41 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Teacher extends Person {
+    private final List<Klass> taughtClasses = new ArrayList<>();
     public Teacher(int id, String name, int age) {
         super(id, name, age);
     }
 
+    public void assignTo(Klass klass) {
+        if (!taughtClasses.contains(klass)) {
+            taughtClasses.add(klass);
+        }
+    }
+
+    public boolean belongsTo(Klass klass) {
+        return taughtClasses.contains(klass);
+    }
+
+    public boolean isTeaching(Student student) {
+        for (Klass klass : taughtClasses) {
+            if (student.isIn(klass)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     @Override
     public String introduce() {
-        return super.introduce() + " I am a teacher.";
+        if (!taughtClasses.isEmpty()) {
+            String classNumbers = taughtClasses.stream()
+                    .map(Klass::getNumber)
+                    .map(String::valueOf)
+                    .collect(Collectors.joining(", "));
+
+            return super.introduce() + " I am a teacher. I teach Class " + classNumbers + ".";
+        } else{
+            return super.introduce() + " I am a teacher.";
+        }
     }
 }
