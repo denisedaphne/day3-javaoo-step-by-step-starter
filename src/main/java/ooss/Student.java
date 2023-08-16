26f8821182df79128da70848e5a493510f5ba6f2
Daphne Balane <balanedaphne20@gmail.com>
2023-08-16 18:51:46 +0800

    feat: implemented codes needed for the Step3 and passed the KlassTest and StudentTest test cases

--- src/main/java/ooss/Student.java
+++ src/main/java/ooss/Student.java
@@ -1,13 +1,25 @@
 package ooss;
 
 public class Student extends Person {
+    private Klass klass;
 
     public Student(int id, String name, int age) {
         super(id, name, age);
     }
 
+    public void join(Klass klass) {
+        this.klass = klass;
+    }
+
+    public boolean isIn(Klass klass) {
+        return this.klass != null && this.klass.equals(klass);
+    }
+
     @Override
     public String introduce() {
+        if (klass != null) {
+            return super.introduce() + " I am a student. I am in class " + klass.getNumber() + ".";
+        }
         return super.introduce() + " I am a student.";
     }
 }
